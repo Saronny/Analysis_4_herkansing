1648ae82e39a28a75d5590e0eae91bb5827aba11
Saronny <101633650+Saronny@users.noreply.github.com>
2022-08-21 01:45:09 +0200

    runBB() unieke pathing array
    de runBB functie bevat nu een beginnetje van een unieke pathing array deze is echter nog een beetje buggy.

--- BB.sh
+++ BB.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+########### GLOBALS ###########
+declare destination
+declare words
+declare -a wordsArray
+declare -a filesToCopy
+
 log() {
     error="there was an error in the execution, for more details check log.txt"
 
@@ -25,26 +31,38 @@ createArchive() {
     mkdir $directory || log "Failed to make a Archive"
 }
 
-
+checkDir() { # Accepteerd 1 directory als argument
+    if [ ! -d "$1" ]
+    then
+        # Dir bestaat niet, geef error naar stderr en doe in log.txt
+        echo "ERROR: \"$1\" is not an existing directory." 2> ~/log.txt # Hoe moet deze error gehandeld worden? Misschien uit function breaken op een of andere manier want "exit 1" sluit de hele console.
+    fi
+}
 
 configureBB() {
     declare OPTARG
     declare arg
     declare OPTIND
-    declare destination
-    declare words
 
-    while getopts ':d:p:' arg
+    unset destination
+    unset words
+
+    while getopts ':d:b:' arg
     do
         case $arg in
         d)
             destination="$OPTARG"
-            echo "$OPTARG"
+            checkDir "$destination"
+            # echo "$OPTARG"
             ;;
 
-        p)
+        b)
             words="$OPTARG"
-            echo "$OPTARG"
+            if [ ! -f "$words" ] # check of file bestaat
+            then
+                echo "ERROR: $words is not an existing file." 2> ~/log.txt # Hoe moet deze error gehandeld worden? Misschien uit function breaken op een of andere manier want "exit 1" sluit de hele console.
+            fi
+            # echo "$OPTARG"
             ;;
 
         \?)
@@ -64,8 +82,47 @@ configureBB() {
             directory="archive$i"
             ((i=i+1))
         done
+        destination="./$directory"
         createArchive
     fi
+
+    if [ -z "$words" ]
+    then
+        wordsArray=("bad")
+        #touch defaultwords.txt
+        #echo "bad" > defaultwords.txt
+        #words="./defaultwords.txt"
+    else
+        # Maak een array van alles wat in de file $words staat
+        mapfile -t wordsArray < "$words"
+    fi
+
+    echo "-d = $destination"
+    echo "-b = ${wordsArray[*]}"
 }
+
+
+runBB() {
+    unset filesToCopy
+    declare -a uniqsArr
+
+    for i in "${wordsArray[@]}"
+    do
+        filesToCopy+=("$(grep -rl "${wordsArray[i]}" "./" )")
+    done
+
+    uniqsArr=($(echo "${filesToCopy[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
+    echo "unique array: "
+    for i in "${uniqsArr[@]}"
+    do
+        echo $i
+    done
+}
+
+# Nog te doen
+# Moet het programma terminaten als de dir niet bestaat?
+# createArchive als destination bestaat
+
+
 
 
